b5921a7ada334cea20b8955fe43e276387ced455
Dan Trapp <dtrapp@gmail.com>
2012-12-07 18:34:15 -0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
 
   it { should be_valid }
 
@@ -105,10 +107,15 @@ end
     describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      #user_with_same_email.email = @user.email.upcase
+      user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
     it { should_not be_valid }
   end
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
 end
